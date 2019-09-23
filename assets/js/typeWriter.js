7abe7a2b552e9fc2d1ed484c05341547215a7bdc
data-insider <datainsiderorg@gmail.com>
2019-09-23 14:59:33 -0400

    Update

--- assets/js/typeWriter.js
+++ assets/js/typeWriter.js
@@ -1,12 +1,31 @@
-var i = 0;
-var txt = '"Once';
-var speed = 80;
+var pos = 0;
+var turn = 0;
+var data = ['Machine Learning','Business Intelligence', 'Data Mining','Data Engineering', 'Deep Learning', 'Big Data Analytics', 'Computer Vision', 'Time-series Analysis', 'Cloud Computing', 'ETL & Ingestion Automation', 'Real-time Processing', 'Recommendation Engines', 'Image Processing', 'Marketing Intelligence', 'Interactive Dashboards'];
+var speed = 90;
+
+setTimeout(typeWriter, speed);
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("introType").innerHTML += txt.charAt(i);
-    i++;
+  if (pos < data[turn].length) {
+    document.getElementById("demo").innerHTML += data[turn].charAt(pos);
+    console.log(data[turn].charAt(pos));
+    pos++;
     setTimeout(typeWriter, speed);
+  } else {
+  	setTimeout(erase, speed+500);
   }
 }
-var myVar = setInterval(typeWriter, 3000);
+
+function erase() {
+	if (pos >= 0) {
+      var str=data[turn].toString().substring(0, pos);
+      document.getElementById("demo").innerHTML = str;
+      pos--;
+      setTimeout(erase, speed-60);
+    } else {
+      turn++;
+      if(turn>=data.length)
+        turn=0;
+      setTimeout(typeWriter, speed);
+    }
+}
