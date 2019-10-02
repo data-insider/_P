b245fd314b4b04ed4d1e6249f39d82f9688f75e3
data-insider <datainsiderorg@gmail.com>
2019-10-02 14:00:49 -0400

    Update

--- assets/js/typeWriter.js
+++ assets/js/typeWriter.js
@@ -1,6 +1,6 @@
 var pos = 0;
 var turn = 0;
-var data = ['              ' ,'Machine Learning','Business Intelligence', 'Data Mining', 'Deep Learning', 'Big Data Analytics', 'Computer Vision', 'Supervised Learning', 'Time-series Analysis', 'ETL Automation', 'Recommendation Engines', 'Image Processing', 'Marketing Intelligence', 'Interactive Dashboards', 'Serverless Computing', 'Distributed Data Pipelines', 'Report Automation', 'User Segmentation', 'Classification & Clustering'];
+var data = ['                     ' ,'Machine Learning','Business Intelligence', 'Data Mining', 'Deep Learning', 'Big Data Analytics', 'Computer Vision', 'Supervised Learning', 'Time-series Analysis', 'ETL Automation', 'Recommendation Engines', 'Image Processing', 'Marketing Intelligence', 'Interactive Dashboards', 'Serverless Computing', 'Distributed Data Pipelines', 'Report Automation', 'User Segmentation', 'Classification & Clustering'];
 var speed = 60;
 
 setTimeout(typeWriter, speed);
